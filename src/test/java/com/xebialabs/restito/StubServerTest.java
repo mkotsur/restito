96582377179bf7169a89b0820cf5ef8bb8977808
Mykhailo Kotsur <miccots@gmail.com>
2012-08-29 22:08:40 +0200

    Finished migration

---
+++ src/test/java/com/xebialabs/restito/StubServerTest.java
@@ -0,0 +1,13 @@
+package com.xebialabs.restito;
+
+import org.junit.*;
+
+import static junit.framework.Assert.assertFalse;
+
+public class StubServerTest {
+
+	@Test
+	public void shouldWork() {
+		assertFalse(false);
+	}
+}
