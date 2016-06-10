af863083e24ee5929a1f25f2b7407680be239243
Mike Kotsur <miccots@gmail.com>
2016-06-10 23:21:09 +0200

    Release automation FTW

---
+++ script/pre-release.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+[ -z "$1" ] && echo 'Error: pass the new version' && exit 1
+
+CURRENT_VERSION=`cat build.gradle  | head -n 1 | grep -o -e '\d\.\d\.\d'`
+
+
+sed -i '' -e "s/$CURRENT_VERSION/$1/g" README.md
+sed -i '' -e "s/$CURRENT_VERSION/$1/g" examples/popular-page/build.gradle
+sed -i '' -e "s/$CURRENT_VERSION/$1/g" examples/popular-page/pom.xml
+sed -i '' -e "s/$CURRENT_VERSION/$1/g" examples/standalone-server/build.gradle
+
+echo "Don't forget to create release notes. This is what has been done up till now"
+
+git log --pretty=oneline --abbrev-commit $CURRENT_VERSION...HEAD
